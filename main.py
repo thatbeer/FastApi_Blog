f34b278243ab81ebc31d0c1635d6623520a6cfe7
thatbeer <88705394+thatbeer@users.noreply.github.com>
2022-10-13 00:24:42 +0700

    update schemas db folder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from xml.etree.ElementInclude import FatalIncludeError
 from fastapi import FastAPI
-
+from db import models
+from db.database import engine
 
 app = FastAPI()
 
@@ -8,3 +9,6 @@ app = FastAPI()
 @app.get("/")
 def hi():
     return "hello world"
+
+
+models.Base.metadata.create_all(engine)
